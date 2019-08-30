77a49e311a388f6123ecf970864ab38f9bf4493d
Jeremy Fisher <jeremy@adamsfisher.me>
2019-08-29 23:28:28 -0400

    .

--- app/__init__.py
+++ app/__init__.py
@@ -1,6 +1,4 @@
 from flask import Flask
-from flask.ext.heroku import Heroku
 app = Flask(__name__, instance_relative_config=True)
-heroku = Heroku(app)
 from app import views
 app.config.from_object('config')
