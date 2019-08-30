ef6eb3e2e4456fa1b1a18e05a8e2a83f5bc6f37b
Jeremy Fisher <jeremy@adamsfisher.me>
2019-08-29 23:26:18 -0400

    trying to get running on heroku

--- app/__init__.py
+++ app/__init__.py
@@ -1,4 +1,6 @@
 from flask import Flask
+from flask.ext.heroku import Heroku
 app = Flask(__name__, instance_relative_config=True)
+heroku = Heroku(app)
 from app import views
 app.config.from_object('config')
